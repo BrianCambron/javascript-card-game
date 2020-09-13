556785b30e2f3b302c7e9aa90d76d7ebb94d74f9
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-13 17:30:23 -0400

    added win condition and started to work on war function

--- main.js
+++ main.js
@@ -54,27 +54,49 @@ class Deck {
 const deck = new Deck(); //<- this needs to be in the game constructors
 
 class Player {
-  constructor({name, cards}={}){
+  constructor({name, cards} = {}) {
     this.name = name;
-    this.hand = deck.cards.splice(0,26);
+    this.hand = deck.cards.splice(0, 26);
   }
 }
 class Game {
-  constructor(){
+  constructor() {
 
   }
 }
 const Player1 = new Player()
 const Player2 = new Player()
+const rewards = []; // this is the reward array, the player who wins war takes these
 
-play = function (p1, p2) {
-//if else to check if each player has cards
-    if (Player1.hand.splice(0, 1) > Player2.hand.splice(0, 1)) {
-        let Player2card = Player2.hand.shift()
-        return Player1.hand.push()
-    } else {
-        let Player1card = Player1.hand.shift()
-        return Player2.hand.push()
-    }
+play = function(p1, p2) {
+
+  //checking to see if players still have cards
+  if (Player1.hand.length === 0) { // game over for player1
+    return 'Player 2 wins!'
+  }
+  if (Player2.hand.length === 0) { //game over for player2
+    return 'Player 1 wins!'
+  }
+  //Comparing card values
+  if (Player1.hand.splice(0, 1) > Player2.hand.splice(0, 1)) {  //compares the cards at index 0 between both hands
+    console.log('Player 1 hand', Player1.hand);
+    console.log('Player 2 hand', Player2.hand);
+    let Player2card = Player2.hand.shift(); // if player1 value is bigger it takes player 2 card
+    return Player1.hand.concat(Player2card); // adds it to the end of the array
+  } else {
+    console.log('Player 1 hand', Player1.hand);
+    console.log('Player 2 hand', Player2.hand);
+    let Player1Card = Player1.hand.shift(); //if player2 value is bigger it takes player 1 card
+    return Player2.hand.concat(Player1Card); //adds it to the end of the array
+  }
+  // War
+  if(Player1.hand.splice(0, 1) === Player2.hand.splice(0,1)){
+    rewards.push(Player1Card);
+    rewards.push(Player2card);
+    console.log(rewards.push(Player1Card));
+    console.log(rewards.push(Player2card));
+    return rewards;
+    console.log(rewards);
+  }
 }
 play();
