499149d0e8dd91dd41add89f2d29bd75440e55d1
BrianCambron <69121396+BrianCambron@users.noreply.github.com>
2020-09-14 15:02:11 -0400

    got war to work

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 class Card {
-  constructor({
-    value,
-    suit
-  }) {
+  constructor({ value, suit}) {
     this.value = value;
     this.suit = suit;
   }
+  // get description(){
+  //   const values = [null, null, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+  //   return `${values[this.value]} of ${this.suit}`;
+  // }
 }
 
 
@@ -54,7 +55,7 @@ class Deck {
 const deck = new Deck(); //<- this needs to be in the game constructors
 
 class Player {
-  constructor({name, cards} = {}) {
+  constructor({name, hand} = {}) {
     this.name = name;
     this.hand = deck.cards.splice(0, 26);
   }
@@ -63,40 +64,103 @@ class Game {
   constructor() {
 
   }
+  
 }
-const Player1 = new Player()
-const Player2 = new Player()
+const player1 = new Player();
+const player2 = new Player();
 const rewards = []; // this is the reward array, the player who wins war takes these
 
 play = function(p1, p2) {
 
   //checking to see if players still have cards
-  if (Player1.hand.length === 0) { // game over for player1
+  if (player1.hand.length === 0) { // game over for player1
     return 'Player 2 wins!'
   }
-  if (Player2.hand.length === 0) { //game over for player2
+  if (player2.hand.length === 0) { //game over for player2
     return 'Player 1 wins!'
   }
   //Comparing card values
-  if (Player1.hand.splice(0, 1) > Player2.hand.splice(0, 1)) {  //compares the cards at index 0 between both hands
-    console.log('Player 1 hand', Player1.hand);
-    console.log('Player 2 hand', Player2.hand);
-    let Player2card = Player2.hand.shift(); // if player1 value is bigger it takes player 2 card
-    return Player1.hand.concat(Player2card); // adds it to the end of the array
-  } else {
-    console.log('Player 1 hand', Player1.hand);
-    console.log('Player 2 hand', Player2.hand);
-    let Player1Card = Player1.hand.shift(); //if player2 value is bigger it takes player 1 card
-    return Player2.hand.concat(Player1Card); //adds it to the end of the array
+  console.log('test', player1.hand.slice(0, 1)[0].value);
+  console.log('test', player2.hand.slice(0, 1)[0].value);
+  console.log('Player 1 hand', player1.hand);
+  console.log('Player 2 hand', player2.hand);
+
+  // player1Card =
+  // player2Card =
+
+  if (player1.hand.slice(0, 1)[0].value > player2.hand.slice(0, 1)[0].value) {  //compares the cards at index 0 between both hands
+    // console.log('Player 1 hand', player1.hand);
+    // console.log('Player 2 hand', player2.hand);
+    let player2Card = player2.hand.shift(); // if player1 value is bigger it takes player 2 card
+    let player1Card = player1.hand.shift();
+    player1.hand.push(player2Card); // adds it to the end of the array
+    player1.hand.push(player1Card);
+  } else if(player2.hand.slice(0, 1)[0].value > player1.hand.slice(0, 1)[0].value){
+    // console.log('Player 1 hand', player1.hand);
+    // console.log('Player 2 hand', player2.hand);
+    let player1Card = player1.hand.shift(); //if player2 value is bigger it takes player 1 card
+    let player2Card = player2.hand.shift();
+    player2.hand.push(player1Card); //adds it to the end of the array
+    player2.hand.push(player2Card);
+  } else if(player1.hand.slice(0, 1)[0].value === player2.hand.slice(0, 1)[0].value){
+    let player1Card = player1.hand.shift();
+    let player2Card = player2.hand.shift();
+    // console.log('here', player1Card);
+    // console.log('there', player2Card);
+    rewards.push(player1Card);
+    rewards.push(player2Card);
+
+    while(player1Card.value === player2Card.value){
+      let faceDown1 = player1.hand.shift();
+      let faceDown2 = player2.hand.shift();
+
+      rewards.push(faceDown1);
+      rewards.push(faceDown2);
+
+
+      player1Card = player1.hand.shift();
+      player2Card = player2.hand.shift();
+
+      rewards.push(player1Card);
+      rewards.push(player2Card);
+    }
+
+
+
+      if (player1Card.value > player2Card.value) {
+        // console.log('player1.hand', player1.hand);
+        player1.hand = player1.hand.concat(rewards);
+        // player1.hand = [...player1.hand, ...rewards]
+        // console.log('a', player1.hand.length);
+
+      } else {
+        // console.log('player2.hand', player2.hand);
+        player2.hand = player2.hand.concat(rewards);
+        // console.log('b', player2.hand.length);
+        // player2.hand = [...player2.hand, ...rewards]
+      }
+
+
+
   }
-  // War
-  if(Player1.hand.splice(0, 1) === Player2.hand.splice(0,1)){
-    rewards.push(Player1Card);
-    rewards.push(Player2card);
-    console.log(rewards.push(Player1Card));
-    console.log(rewards.push(Player2card));
-    return rewards;
-    console.log(rewards);
-  }
-}
-play();
+};
+
+
+const game = new Game();
+game.play();
+
+// play();
+//   this.player1.hand = this.deck.cards.filter((card, index) => index % 2 === 0);
+//   this.player2.hand = this.deck.cards.filter((card, index) => index % 2 !== 0)
+// }
+// draw(){
+//   let player1card = this.player1.hand.shift();
+//   let player2card = this.player2.hand.shift();
+//   console.log(`
+// ${this.player1.name} drew ${player1card.description}
+// ${this.player1.name} drew ${{player1card.description}}`
+// );
+// }
+  // while(player1.hand.length > 0 && player2.hand.length > 0){
+  //
+  // }
